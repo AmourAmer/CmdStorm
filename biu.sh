cb0492ef098c0bb2371d1e7f8ef9779767b61d55
AmourAmer <153876579+AmourAmer@users.noreply.github.com>
2024-02-11 15:14:39 +0800

    输入法，可用

--- biu.sh
+++ biu.sh
@@ -1,8 +1,29 @@
 #/bin/bash
-# install with paru or something else
-paru -S fcitx5-im fcitx5-rime rime-ice-git
+echo() {
+	command echo "#####################"
+	command echo "#" $@
+	command echo
+}
+
+安装() {
+	if command -v paru >/dev/null; then
+		paru -S $@
+	elif command -v yay >/dev/null; then
+		yay -S $@
+	elif command -v paru >/dev/null; then
+		sudo pacman -S $@
+	else
+		echo 怎么连pacman都没有？目前本项目只支持arch系发行版
+		exit 1
+	fi
+}
+
+# 引导语
+安装 fcitx5-im fcitx5-rime rime-ice-git
 # from website https://wiki.archlinux.org/title/Fcitx5
-# add GTK_IM_MODULE=fcitx
+# add
+# GTK_IM_MODULE=fcitx
 # QT_IM_MODULE=fcitx
 # XMODIFIERS=@im=fcitx
-# either have look at /home/hong/.config/fcitx5/ or settings/Regional Settings/Input Method/Configure global options/Trigger Input Method(Or maybe sth else)
+echo 输入法已经安装好咯，登出再重进就可以快乐地输入中文啦～
+# either have look at ~/.config/fcitx5/ or settings/Regional Settings/Input Method/Configure global options/Trigger Input Method(Or maybe sth else)
