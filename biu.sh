bf2ee96b663e32783cbbc03dfb21fc5b0c6a4089
AmourAmer <153876579+AmourAmer@users.noreply.github.com>
2024-02-11 12:58:57 +0800

    输入法 raw

---
+++ biu.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+# install with paru or something else
+paru -S fcitx5-im fcitx5-rime rime-ice-git
+# from website https://wiki.archlinux.org/title/Fcitx5
+# add GTK_IM_MODULE=fcitx
+# QT_IM_MODULE=fcitx
+# XMODIFIERS=@im=fcitx
+# either have look at /home/hong/.config/fcitx5/ or settings/Regional Settings/Input Method/Configure global options/Trigger Input Method(Or maybe sth else)
