92972648f62016df2f600297c2affeb4ddbe2650
AmourAmer <153876579+AmourAmer@users.noreply.github.com>
2024-04-07 22:50:16 +0800

    fish

--- src/biu.sh
+++ src/biu.sh
@@ -2,6 +2,7 @@
 # TODO maybe there're some apis to conf
 # TODO add fcitx5-rime to profile and apply
 # TODO show more info
+nix-env -iA nixos.fish # TODO nixos?
 kill $(pgrep fcitx5)
 # TODO be less aggressive, merge, not replace
 # TODO CMD_STORM_PATH
@@ -9,3 +10,4 @@ cp $CMD_STORM_PATH/资源/profile ~/.config/fcitx5/profile
 # TODO 默认简体
 echo 输入法即将就绪。使用ctrl+空格来切换输入法输入，ctrl+\`来切换为简体
 fcitx5 >>/dev/null 2>&1 &
+fish # TODO -C
