c7e12dd772584ff1edfc4c11e5f749bd0fdb7168
AmourAmer <153876579+AmourAmer@users.noreply.github.com>
2024-04-09 23:37:44 +0800

    lua generate conf files

--- lua/main.lua
+++ lua/main.lua
@@ -1,8 +1,59 @@
-local util = require("./util")
+package.path = package.path .. ";./?.lua" -- why lua of nix doesn't include this?
+local util = require("util")
 local profile = util.require("profile")
+local F = {}
+
+-- TODO how about conflicts?
+-- TODO maybe I should split into files
+function F.fish(conf)
+	-- TODO an option to toggle put in the conf or in a .fish file?
+	local config_fish = ""
+	-- alias
+	config_fish = config_fish .. "if status is-interactive\n"
+	for app, content in pairs(conf) do
+		if content["alias"] then
+			for _, v in pairs(content["alias"]) do
+				config_fish = config_fish
+					.. ("alias " .. v[1] .. " " .. v[2]) -- 缩进什么的，管不了了，要么用啥玩意儿来fmt一下
+					.. " # CmdStorm因"
+					.. app
+					.. "的选项自动生成\n"
+			end
+		end
+	end
+	config_fish = config_fish .. "end\n"
+	print(
+		".config/fish/config.fish\n"
+			.. config_fish
+			.. "\n华丽丽的分割线，就靠这行来分文件了，不可能故意跟我重吧？救命啊，要不是我不会数lua的字符串里的回车数岂能这么憋屈\n"
+	)
+end
+
+states = {}
+
+-- use configs to compute final state
 for key, value in pairs(profile) do
 	-- TODO require failure? unused value? output to where?
 	if value then
-		util.require("src." .. key).generate(value)
+		-- TODO shouldn't do this, should have sort of acl
+		state = util.require("src." .. key).generate(value)
+		for k, v in pairs(state) do
+			-- TODO there is potential type problem
+			if states[k] then
+				states[k][key] = v
+			else
+				states[k] = {}
+				states[k][key] = v
+			end
+		end
 	end
 end
+
+-- TODO and where should user funcs be used?
+-- generate files from values of state
+-- TODO value should have a more appropriate name
+for app, value in pairs(states) do
+	F[app](value)
+end
+
+-- TODO record managed files. apply patches
--- lua/src/LazyVim.lua
+++ lua/src/LazyVim.lua
@@ -5,13 +5,20 @@ function M.generate(conf)
 	local nvim_appname = conf.nvim_appname or "lavi"
 	-- TODO check match? notify? and I remember that there's something more. Ah! listen on unknown cmd `lavi`. Where to put alias? add to config.fish? should add a util! add maybe need git mirror?
 	-- TODO what if local/share conflict?
+	-- TODO brand new user must be missing tools to properly run
 	os.execute([[
 if [ ! -d ~/.config/]] .. nvim_appname .. [[ ]
 then
   git clone --depth 1 https://github.com/LazyVim/starter ~/.config/]] .. nvim_appname .. [[ 
   rm -rf ~/.config/]] .. nvim_appname .. [[/.git
 fi ]])
-	-- shit, I lost this file when using lazygit! Also tons of time. Not quite sure if I missed anything
+	local alias = { { nvim_appname, '"NVIM_APPNAME=' .. nvim_appname .. ' nvim"' } }
+	-- TODO shit, I lost this file when using lazygit! Also tons of time. Not quite sure if I missed anything
+	return {
+		fish = {
+			alias = alias,
+		},
+	}
 end
 
 return M
