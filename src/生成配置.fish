8df288a6acbf0adb024ccc436868a00680642626
AmourAmer <153876579+AmourAmer@users.noreply.github.com>
2024-04-11 23:06:25 +0800

    gentle

--- src/生成配置.fish
+++ src/生成配置.fish
@@ -12,7 +12,9 @@ return M" >>~/.config/CmdStorm/lua/profile.lua
     cd $CMD_STORM_PATH/lua/ || return 4
     # split, check, generate, 
     set -l start 1
+    set -l file_paths
     set -l file_path
+    set -l file_contents
     set -l file_content ''
     while read line
         if test $start -eq 1
@@ -23,14 +25,20 @@ return M" >>~/.config/CmdStorm/lua/profile.lua
             set file_content ''
             set start 0
         else if test $line = "华丽丽的分割线，就靠这行来分文件了，不可能故意跟我重吧？救命啊，要不是我不会数lua的字符串里的回车数岂能这么憋屈"
-            # 确实有点脱裤子放屁的感觉，早知道我是这么用声明占有的我就不从文件读了，但也不一定，谁知道呢
-            if 确认占有 "$HOME/$file_path" # 可恶！为什么不能用~！
-                声明占有 (echo $file_content | psub) >"$HOME/$file_path"
-            end
+            set -a file_paths $file_path # Hope that space won't break nothing
+            set -a file_contents $file_content # Hope that space won't break nothing, either
             set start 1
         else
             set file_content "$file_content"\n"$line"
         end
     end <(lua main.lua | psub)
+    for i in (seq (count $file_paths))
+        set -l file_path $file_paths[$i]
+        set -l file_content $file_contents[$i]
+        # 确实有点脱裤子放屁的感觉，早知道我是这么用声明占有的我就不从文件读了，但也不一定，谁知道呢
+        if 确认占有 "$HOME/$file_path" # 可恶！为什么不能用~！
+            声明占有 (echo $file_content | psub) >"$HOME/$file_path"
+        end
+    end
     cd - # TODO 如果你本身就在lua文件夹里面，那么你会被带走，是一个懒得解决的bug
 end
