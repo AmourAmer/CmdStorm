6c9ae3e3895a7a113f33cd68ef139b9f39de472e
AmourAmer <153876579+AmourAmer@users.noreply.github.com>
2024-04-11 14:21:51 +0800

    uninstall

---
+++ 卸载.fish
@@ -0,0 +1,3 @@
+source $CMD_STORM_PATH/entry.fish
+提示 把source \$CMD_STORM_PATH/entry.fish从~/.config/fish/config.fish里去掉（如果你没做其他奇怪的事情） 没错，你得手动去掉，要么等别人贡献更新这个脚本，反正我现在不打算自动化
+提示 当然这并没有删掉你电脑上的相关文件，并不打算告诉你他们藏在了哪里。也不能说藏，我还是很遵循规范的。如果你不知道的话我建议你还是运行fish \$CMD_STORM_PATH/安装CmdStorm.fish装回去
